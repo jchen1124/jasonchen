404f282314ae9191d78ed1331b4669d7b10f86f8
Jason Chen <jchen33@umbc.edu>
2026-06-11 15:07:24 -0400

    Wrap Education section in FadeIn component for improved animation effect

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,13 +1,19 @@
 import Education_Card from "./Education_Card";
+import FadeIn from "./FadeIn";
 import "../styles/Education.css";
 
 const Education = () => {
   return (
     <div id="education">
-      <div className="section-head">
-        <span className="section-title">/ Education</span>
-      </div>
-      <Education_Card />
+      <FadeIn>
+        <div className="section-head">
+          <span className="section-title">/ Education</span>
+        </div>
+      </FadeIn>
+
+      <FadeIn delay={140}>
+        <Education_Card />
+      </FadeIn>
     </div>
   );
 };
