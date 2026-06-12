76f978aee109a8d253405eeabb4a50a171a767f3
Jason Chen <jchen33@umbc.edu>
2026-06-12 18:00:15 -0400

    edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 ">
       <div className="col-md-4 d-flex align-items-center">
         <span className="mb-3 mb-md-0 ">
-          © 2025 Jason Chen. All rights reserved.
+          © Jason Chen <strong>All rights reserved.</strong>
         </span>
       </div>
       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex gap-3">
