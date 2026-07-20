e207dcf22367009e04a08124ef75ca210c08a78f
Jason Chen <jchen33@umbc.edu>
2026-07-20 16:05:41 -0400

    Add Wordle component with game mode functionality and styles

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../styles/Wordle.css";
 
 const Wordle = () => {
   const [isGameMode, setGameMode] = useState(false);
@@ -20,4 +21,7 @@ const Wordle = () => {
 
     </div>
   );
+
 };
+
+export default Wordle
