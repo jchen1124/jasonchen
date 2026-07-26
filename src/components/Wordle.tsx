80503ec74400270a15d87918339552fb8dabf932
Jason Chen <jchen33@umbc.edu>
2026-07-26 19:22:45 -0400

    - Implemented short get random word function -Adjusted start and eng game logic - Edit wordle file

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import { ANSWER_WORDS } from "../data/wordleWords";
 import "../styles/Wordle.css";
+
+const getWord = () => {
+    const randomIndex = Math.floor(Math.random() * ANSWER_WORDS.length)
+    return ANSWER_WORDS[randomIndex]
+  }
 
 const Wordle = () => {
   const [isGameMode, setGameMode] = useState(false);
+  const [targetWord, setTargetWord] = useState<string | null>(null)
+  
+  const startGame = () => {
+    setGameMode(true);
+    setTargetWord(getWord());
+  }
 
+  const endGame = () => {
+    setGameMode(false);
+    setTargetWord(null);
+  }
   return (
     <div className="game-mode-wrap">
       <button
         className={`game-mode ${isGameMode ? "active" : ""}`}
-        onClick={() => setGameMode((current) => !current)}
+        onClick={isGameMode ? endGame : startGame}
       >
         <span>{isGameMode ? "End Game" : "Game Mode"}</span>
       </button>
